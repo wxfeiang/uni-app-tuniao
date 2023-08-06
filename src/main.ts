f2d02da05fe0de8a4900ca0fa62b0d4dff6407d4
王鹏2017 <mac@wangpengdeMacBook-Pro-2017.local>
2023-08-06 16:28:22 +0800

    feat:新增pinia修改报错

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import * as Pinia from "pinia"
+import { createSSRApp } from "vue"
+import App from "./App.vue"
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App)
+  app.use(Pinia.createPinia())
   return {
     app,
-  };
+    Pinia
+  }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,39 +1,36 @@
 <script setup lang="ts">
-const title = ref("Helloadafasadcadasdsdfbkj");
+const a = ref("a");
+import { useCounterStore } from "@/stores/counter";
+const counter = useCounterStore();
+counter.count++;
+// counter.$patch({ count: counter.count + 1 });
+// 或使用 action 代替
+
+const add = () => {
+  counter.increment();
+};
+
+const list = ref([
+  {
+    text: "点赞",
+    color: "blue",
+    fontSize: 28,
+  },
+  {
+    text: "分享",
+  },
+  {
+    text: "评论",
+  },
+]);
+
+const show = ref(true);
 </script>
-
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+  {{ a }}==========
+  <view type="primary" @click="add">操作pinia</view>
+  <!-- 直接从 store 中访问 state -->
+  <view>Current Count: {{ counter.count }}</view>
+  222
+  <u-action-sheet :list="list" v-model="show"></u-action-sheet>
 </template>
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
