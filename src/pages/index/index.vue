9bc17575cbaa9055043fca8b7d37e4ec9ec21201
wxfeiang <wxfeiang@qq.com>
2023-08-06 21:47:55 +0800

    style: 更新组件库的使用

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { useCounterStore } from "@/stores/counter";
-import uActionSheet from "uview-plus/components/u-action-sheet/u-action-sheet.vue";
 
 const a = ref("afffff");
 const counter = useCounterStore();
@@ -29,11 +28,23 @@ const list = ref([
 const show = ref(true);
 </script>
 <template>
-  {{ a }}=========={{list}}
+  {{ a }}=========={{ list }}
   <view type="primary" @click="add">操作pinia</view>
   <!-- 直接从 store 中访问 state -->
   <view>Current Count: {{ counter.count }}</view>
   <u-icon name="photo"></u-icon>
   <u-action-sheet :list="list" v-model="show"></u-action-sheet>
   <u-button text="月落"></u-button>
+  <up-button type="primary" text="确定"></up-button>
+  <up-button type="primary" :plain="true" text="镂空"></up-button>
+  <up-button type="primary" :plain="true" :hairline="true" text="细边"></up-button>
+  <up-button type="primary" :disabled="disabled" text="禁用"></up-button>
+  <up-button type="primary" loading loadingText="加载中"></up-button>
+  <up-button type="primary" icon="map" text="图标按钮"></up-button>
+  <up-button type="primary" shape="circle" text="按钮形状"></up-button>
+  <up-button
+    text="渐变色按钮"
+    color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
+  ></up-button>
+  <up-button type="primary" size="small" text="大小尺寸"></up-button>
 </template>
