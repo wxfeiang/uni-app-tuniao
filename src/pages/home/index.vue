45e87bfcba4d6ac41cceb9f903c42fd83d75a515
wxfeiang <wxfeiang@qq.com>
2023-11-14 23:26:12 +0800

    feat: 添加自定义tabbar

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,20 +1,49 @@
 <script lang="ts" setup>
-const { Login, userInfo, tesToken, loginFrom, rules } = useAuth();
-const router = useRouter();
+import { ref } from "vue";
 
-//FIX: 只能在 vue 中使用路由插件
-function goto() {
-  router.push({ name: "test" });
-}
+const currentTabbar = ref(0);
+
+// 导航栏数据
+const tabbarData = [
+  {
+    name: "首页",
+    icon: "home",
+    activeIcon: "home-fill",
+  },
+  {
+    name: "爱好",
+    icon: "like",
+    activeIcon: "like-fill",
+  },
+  {
+    name: "积分",
+    icon: "shop",
+    activeIcon: "shop-fill",
+  },
+  {
+    name: "热谤",
+    icon: "fire",
+    activeIcon: "fire-fill",
+  },
+  {
+    name: "我的",
+    icon: "my-circle",
+    activeIcon: "my-circle-fill",
+  },
+];
 </script>
 
 <template>
-  <tn-button @click="Login">提交</tn-button>
-
-  <view class="tn-text-ellipsis-1">
-    sv sfvsdfoivhso'difjvopshfviousdf povjs pofvosifnvpo sjf iopvwa'oifhvo aejfv [osanf pi
-  </view>
-
-  <tn-button @click="tesToken">测试登录</tn-button>
-  <tn-button @click="goto">路由跳转</tn-button>
+  <tn-tabbar v-model="currentTabbar" fixed switch-animation>
+    <!-- 导航栏 -->
+    <tn-tabbar-item
+      v-for="(item, index) in tabbarData"
+      :key="index"
+      :icon="item.icon"
+      :active-icon="item.activeIcon"
+      :text="item.name"
+      :bulge="index === 2"
+      bulge-bg-color="tn-gradient__cool-6"
+    />
+  </tn-tabbar>
 </template>
